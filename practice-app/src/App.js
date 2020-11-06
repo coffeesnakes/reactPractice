4e65127e458ae74ac35b9b601ff616fdbae7954c
Jacob Berg <coffeesnakes@gmail.com>
2020-11-06 13:46:02 -0800

    base react files

---
+++ practice-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ayy wadap
+        </p>
+        <p>
+         test "p-2"
+        </p>
+
+        <p> are you ok??
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
