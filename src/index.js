39b389edb537efac5996da89baf10847599dff83
Jacob Berg <coffeesnakes@gmail.com>
2020-11-23 18:05:45 -0800

    enterName y button

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 //imports react from node_modules directory and gets all the code from inside.
 
 const App = () => {
-  return <div> Hello friend</div>
+  return ( <div>
+    <label class="label" for="name">Enter name:</label>
+    <input id="name" type="text" />
+    <button
+    style={{
+      backgroundColor: 'pink',
+      color: 'white'
+    }}> Submit
+    </button>
+    </div>
+
+    )
 };
 
-// shows react component on the screen with render by referencing root in index.html
+// shows react component on the screen with render
+// by referencing root in index.html
 ReactDOM.render(
   < App />, document.querySelector('#root')
 );
